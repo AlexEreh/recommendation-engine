f32de02e57b34ad121c4d6a4493fd97cb48ab2ee
Alexander Ereshkin <xcode111@mail.ru>
2024-04-14 13:51:53 +0300

    Add results

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ from content_simple import get_content_simple_recs
 def main():
     movies_df: DataFrame = (pl.read_csv('dataset/movies_metadata.csv', infer_schema_length=36000)
                             .select(pl.col("title", "overview", "vote_average", "vote_count", "genres")))
+    print("---Результат работы простейшего алгоритма---")
     print(get_content_simple_recs(movies_df, 10))
+    print("---Результат работы алгоритма на базе TF-IDF---")
     print(get_content_tfidf_recs(movies_df, 'The Dark Knight Rises').head(10))
+    print("---Результат работы алгоритма на базе заготовленных ключевых слов---")
     print(get_content_keywords_recs('The Dark Knight Rises').head(10))
 
 
